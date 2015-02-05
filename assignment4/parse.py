863e96baea15056bdcccffb15169e4457cdaf543
Danya Lagos <tollibaa@gmail.com>
2015-02-04 22:31:47 -0600

    Made a mistake in saving files. Saving it here.

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -12,5 +12,30 @@ def readCSV(filename):
         lines = list(rdr)
     return(lines)
 
-
 ### enter your code below
+def get_avg_latlng():
+    '''Computes the average latitude and longitude of construction permits in HP,
+    prints it to the console'''
+    hppermits = readCSV('permits_hydepark.csv')
+    SumLong=0
+    SumLat=0
+    for row in hppermits:
+        SumLong += float(row[127])
+        SumLat += float (row[128])
+
+        avgLong = sumLong/len(permits)
+        avgLat = sumLat/len(permits)
+    return avgLong, avgLat
+get_avg_latlng()
+
+def zip_code_barchart(x):
+    zipcode_list = []
+    for i in range (len(x)):
+        zipcode = x[i][28]
+        zipcode_list.append(zipcode)
+
+    plt.hist(zipcode_list, bins=10)
+    plt.title("Zip Codes")
+    img.save("barchart.jpg")
+
+zip_code_barchart(permits)
