0af1586672ecc3967e7e063bd50aec4ee5a72e5d
Danya Lagos <tollibaa@gmail.com>
2015-02-09 15:37:26 -0600

    Monday, 3:37PM.

--- Assignment5/fetchArtist.py
+++ Assignment5/fetchArtist.py
@@ -6,7 +6,12 @@ def fetchArtistId(name):
     """Using the Spotify API search method, take a string that is the artist's name, 
     and return a Spotify artist ID.
     """
-    pass
+    name = name().replace('', '%20')
+    url ="https://api.spotify.com/v1/search?q=" + name + "&type=artist"
+    req = requests.get(url)
+    print(req)
+    print(type(req))
+
 
 def fetchArtistInfo(artist_id):
     """Using the Spotify API, takes a string representing the id and
@@ -15,3 +20,4 @@ def fetchArtistInfo(artist_id):
     """
     pass
 
+fetchArtistId(sys.argv[1])
