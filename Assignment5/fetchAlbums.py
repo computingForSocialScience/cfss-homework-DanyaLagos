d1a67bd69eb2757b6447c524ab153c2bcafca6ee
Danya Lagos <tollibaa@gmail.com>
2015-02-10 11:03:31 -0600

    What you need to do here is basically match fetchArtist functions to albums.

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -5,14 +5,23 @@ def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
     returns a list of album IDs in a list
     """
-    url = "https://api.spotify.com/v1/artists/" + artist_id
+    url = "https://api.spotify.com/v1/artists/" + artist_id + '/albums?market=US&album_type=album'
 
     req = requests.get(url)
     assert req.ok, 'No record found.'
 
     dict = req.json()
-    assert dict.get('artist_id'), 'Artist not found.'
+    assert dict.get('name'), 'Album not found.'
 
+    keys = {}
+    keys['followers'] = dict['followers']['total']
+    keys['genres'] = dict['genres']
+    keys['id'] = dict['id']
+    keys['name'] = dict['name']
+    keys['popularity'] = dict['popularity']
+    
+    print(keys)
+    return(keys)
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
@@ -20,3 +29,4 @@ def fetchAlbumInfo(album_id):
     """
     pass
 
+fetchAlbumIds('2BTZIqw0ntH9MvilQ3ewNY')
