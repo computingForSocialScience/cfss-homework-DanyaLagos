313fe5777581b2140b289e5640682abaaca0b1a0
Danya Lagos <tollibaa@gmail.com>
2015-02-10 10:29:45 -0600

    Progress 10:30AM. Working off of fetchArtistInfo, exchanging name for artist_id where possible.

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -5,7 +5,14 @@ def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
     returns a list of album IDs in a list
     """
-    pass
+    url = "https://api.spotify.com/v1/artists/" + artist_id
+
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+
+    dict = req.json()
+    assert dict.get('artist_id'), 'Artist not found.'
+
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
