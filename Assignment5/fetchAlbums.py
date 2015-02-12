c7d34bfda97b80878f462195cdf9a1993ef195b7
Danya Lagos <tollibaa@gmail.com>
2015-02-11 23:54:45 -0600

    still working on this.

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -6,6 +6,20 @@ def fetchAlbumIds(artist_id):
     returns a list of album IDs in a list
     """
     url = "https://api.spotify.com/v1/artists/" + artist_id + '/albums?market=US&album_type=album'
+    
+    req = requests.get(url)
+    assert req.ok, 'No record found.'
+    
+    dict = req.json()
+    assert dict.get('artists').get('items'), 'No albums found.'
+    
+    return[album]
+
+def fetchAlbumInfo(album_id):
+    """Using the Spotify API, take an album ID 
+    and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
+    """
+    url = "https://api.spotify.com/v1/artists/" + artist_id + '/albums?market=US&album_type=album'
 
     req = requests.get(url)
     assert req.ok, 'No record found.'
@@ -20,13 +34,9 @@ def fetchAlbumIds(artist_id):
     keys['name'] = dict['name']
     keys['popularity'] = dict['popularity']
     
-    print(keys)
+    #print(keys)
     return(keys)
 
-def fetchAlbumInfo(album_id):
-    """Using the Spotify API, take an album ID 
-    and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
-    """
-    pass
+    pass 
 
 fetchAlbumIds('2BTZIqw0ntH9MvilQ3ewNY')
