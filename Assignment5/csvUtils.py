a278c5737ffc33a2bdf4e3f951de59f928a0e50a
Danya Lagos <tollibaa@gmail.com>
2015-02-12 01:18:00 -0600

    1:17AM progress.

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -1,4 +1,5 @@
 from io import open
+import csv 
 
 def writeArtistsTable(artist_info_list):
     """Given a list of dictionries, each as returned from 
@@ -7,7 +8,18 @@ def writeArtistsTable(artist_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY
     """
-    pass 
+
+    f = open('artists.csv', 'w', encoding='utf-8')
+    try:
+        f.write(u'ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY\n')
+        for keys in artist_info_list:
+            artist_id = keys['id']
+            artist_name = keys['name']
+            artist_followers = keys['followers']
+            artist_popularity = keys['popularity']
+            f.write(u'%s,"%s",%d,%d\n' % (artist_id, artist_name, artist_followers, artist_popularity))
+    finally:
+        f.close()
       
 def writeAlbumsTable(album_info_list):
     """
@@ -18,4 +30,13 @@ def writeAlbumsTable(album_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
-    pass
+
+    f = open('albums.csv,' 'w', encoding='utf-8')
+    try:
+        f.write(u'ARTIST_ID, ALBUM_ID,ALBUM_NAME,ALBUM_POPULARITY\n')
+        for keys in album_info_list:
+            artist_id = keys['artist_id']
+            album_id = keys['album_id']
+            name = keys['name']
+            year = keys['year']
+            popularity = keys['popularity']
