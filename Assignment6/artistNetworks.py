f082d3472f2ae1a2321daf83d7f2cfe39bd6800b
Danya Lagos <tollibaa@gmail.com>
2015-02-14 15:57:40 -0600

    Part I.1 is done.

--- Assignment6/artistNetworks.py
+++ Assignment6/artistNetworks.py
@@ -1,7 +1,21 @@
+import requests
 '''Function I.1:
 This function takes an artist ID as its only argument 
 and returns a list of related artists IDs.'''
 def getRelatedartists(artistID):
+	url = "https://api.spotify.com/v1/artists/" + artistID + "/related-artists"
+	req = requests.get(url)
+	assert req.ok, 'No record found.'
+	data = req.json()
+	assert data.get('artists'), 'No artist found.'
+
+	relatedartist_List = [] #Building dictionary that will be returned.
+	for relatedartist in data['artists']:
+		relatedartist_List.append(relatedartist['id'])
+
+	return relatedartist_List
+
+print getRelatedartists("0OdUWJ0sBjDrqHygGUXeCF")
 
 
 '''Function I.2:
@@ -11,7 +25,7 @@ related artists.
 Make sure your list of edges only lists each edge once, with no duplicates.'''
 
 def getDepthEdges(artistID, depth):
-
+	pass 
 
 '''Function I.3:
 This function takes the exact same arguments as getDepthEdges(), 
@@ -21,7 +35,7 @@ a data frame with 305 rows and two columns.
 This should be a simple function that just calls getDepthEdges() 
 and then loads the results into a data frame.'''
 def getEdgeList(artistID, depth):
-
+	pass
 
 '''Function 1.4: This function takes three arguments: 
 an artist ID, a depth value, and a filename for output. 
@@ -29,3 +43,4 @@ This function should generate an edge list based on the parameters
 artistId and depth, and write that to a CSV file specified by the 
 filename parameter. '''
 def writeEdgeList(artistID, depth, filename):
+	pass 
