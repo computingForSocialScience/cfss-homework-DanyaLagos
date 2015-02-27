41da1bce85ea7ded4258d95de2a201618a8a2e43
Danya Lagos <tollibaa@gmail.com>
2015-02-27 14:23:21 -0600

    updated

--- Assignment6/makePlaylist.py
+++ Assignment6/makePlaylist.py
@@ -2,10 +2,7 @@ import sys
 import pandas
 import networkx
 import random
-from artistNetworks import *
-from analyzeNetworks import *
-from fetchArtist import *
-from fetchAlbums import *
-
-if __name__ == '__main__': 
-	artist_names = sys.argv
+import artistNetworks
+import analyzeNetworks
+import fetchArtist
+import fetchAlbums
