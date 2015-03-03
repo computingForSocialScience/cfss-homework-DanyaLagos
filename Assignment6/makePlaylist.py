792627289db060b77864c2c550a2ef1b9a9684c5
Danya Lagos <tollibaa@gmail.com>
2015-03-03 10:28:00 -0600

    getting close to finishing.

--- Assignment6/makePlaylist.py
+++ Assignment6/makePlaylist.py
@@ -2,7 +2,42 @@ import sys
 import pandas
 import networkx
 import random
-import artistNetworks
-import analyzeNetworks
-import fetchArtist
-import fetchAlbums
+import csv 
+import requests
+from io import open
+from artistNetworks import *
+from analyzeNetworks import *
+from fetchArtist import *
+from fetchAlbums import *
+
+if __name__ == '__main__':
+	artists = sys.argv[1:]
+
+artistids = []
+for name in artists:
+	artistID = fetchArtistID(name)
+	artistids.append(artistID)
+
+edgelists = []
+for i in range(len(artistids)):
+	edgelist = getEdgeList(artistids[i], 2)
+	edgelists.append(edgelist)
+
+mergedlists = edgelists[0]
+for i in range (len(edgelists)):
+	mergedlists = combineEdgeLists(mergedlists, edgelists[i])
+mergedlists.columns = ['artist1', 'artist2']
+
+g = pandasToNetworkX(medgedlists)
+
+sample30 = []
+for i in range(30):
+	sample30.append(randomCentralNote(g))
+
+nameslist = []
+albumlist = []
+
+for artistID in sample30:
+	name = fetchArtistInfo(artistId)['name']
+	
+
