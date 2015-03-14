07d60b8a52cbc429c5cf90df5ec1cadde2b03d97
Danya Lagos <tollibaa@gmail.com>
2015-03-14 09:55:43 -0500

    app.py makes a website now.

--- final_project/app.py
+++ final_project/app.py
@@ -1,2 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pymysql
+
+app = Flask(__name__) #This creates the app object. 
+
+@app.route('/') #Creates the text of a web page. 
+def make_index_resp():
+	return(render_template('index.html'))
+
+if __name__ == '__main__': #tells the app to start. 
+	app.run()
